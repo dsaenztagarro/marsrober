655cb3f71dfdb06c49c5130351f5814226072706
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-23 23:17:01 +0200

    Added pending steps definitions

---
+++ features/step_definitions/marsrover_steps.rb
@@ -0,0 +1,16 @@
+Given(/^a plateau on Mars with dimensions "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^NASA deploys "([^"]*)" to position "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^NASA sends instructions "([^"]*)" to "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^it is expected to find "([^"]*)" at position "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
