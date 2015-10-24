e355109a9822e15be470878e1cf2b5633ee433ec
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-24 14:07:25 +0200

    Added model position

---
+++ lib/position.rb
@@ -0,0 +1,70 @@
+# Represents the position of a robotic rover over the plateau of Mars
+class Position
+  attr_reader :height, :width
+
+  # @param width [Fixnum]
+  # @param height [Fixnum]
+  # @param orientation [Orientation]
+  def initialize(width, height, orientation)
+    @width = width
+    @height = height
+    @orientation = orientation
+  end
+
+  # Applies instruction over it
+  # @param instruction [Instruction]
+  def apply(instruction)
+    send "apply_#{instruction}"
+  end
+
+  # Initializes copy of object for DEEP copy
+  def initialize_copy(source)
+    super
+    @orientation = @orientation.dup
+  end
+
+  def to_s
+    "#{@width} #{@height} #{@orientation.to_char}"
+  end
+
+  def self.zero
+    Position.new(0, 0, Orientation.build('N'))
+  end
+
+  private
+
+  # Instruction commands
+
+  def apply_spin_left
+    @orientation.spin_left
+    self
+  end
+
+  def apply_spin_right
+    @orientation.spin_right
+    self
+  end
+
+  def apply_move_forward
+    send "move_forward_#{@orientation}"
+    self
+  end
+
+  # Orientation commands
+
+  def move_forward_north
+    @height += 1
+  end
+
+  def move_forward_east
+    @width += 1
+  end
+
+  def move_forward_south
+    @height -= 1
+  end
+
+  def move_forward_west
+    @width -= 1
+  end
+end
---
+++ spec/position_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+
+describe Position do
+  let(:orientation) { double('orientation') }
+  let(:instruction) { double('instruction') }
+
+  before :each do
+    @obj = Position.new(1, 1, orientation)
+  end
+
+  describe '#to_s' do
+    before :each do
+      orientation.stub(:to_char).and_return('N')
+    end
+
+    it 'returns the string representation successfully' do
+      expect(@obj.to_s).to eq('1 1 N')
+    end
+  end
+
+  describe '#apply' do
+    %w(left right).each do |direction|
+      context "when instruction spin #{direction}" do
+        it 'applies successfully instruction' do
+          method = "spin_#{direction}"
+          expect(instruction).to receive(:to_s).and_return(method)
+          expect(orientation).to receive(method.to_sym)
+          @obj.apply(instruction)
+        end
+      end
+    end
+
+    context 'when instruction move forward NORTH' do
+      it 'applies successfully instruction' do
+        expect(orientation).to receive('to_s').and_return('north')
+        expect(instruction).to receive('to_s').and_return('move_forward')
+        @obj.apply(instruction)
+        expect(@obj.height).to eq(2)
+      end
+    end
+
+    it 'applies successfully instruction to move forward NORTH' do
+      expect(orientation).to receive('to_s').and_return('north')
+      expect(instruction).to receive('to_s').and_return('move_forward')
+      @obj.apply(instruction)
+      expect(@obj.height).to eq(2)
+    end
+
+    it 'applies successfully instruction to move forward SOUTH' do
+      expect(orientation).to receive('to_s').and_return('south')
+      expect(instruction).to receive('to_s').and_return('move_forward')
+      @obj.apply(instruction)
+      expect(@obj.height).to eq(0)
+    end
+
+    it 'applies successfully instruction to move forward WEST' do
+      expect(orientation).to receive('to_s').and_return('west')
+      expect(instruction).to receive('to_s').and_return('move_forward')
+      @obj.apply(instruction)
+      expect(@obj.width).to eq(0)
+    end
+
+    it 'applies successfully instruction to move forward EAST' do
+      expect(orientation).to receive('to_s').and_return('east')
+      expect(instruction).to receive('to_s').and_return('move_forward')
+      @obj.apply(instruction)
+      expect(@obj.width).to eq(2)
+    end
+  end
+
+  describe '.zero' do
+    it 'returns a new position at origen' do
+      position = Position.zero
+      expect(position.width).to eq(0)
+      expect(position.height).to eq(0)
+    end
+  end
+end
