7b7887889d5d4662799b14c3e25a7e58b69ebca6
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-10-24 14:08:22 +0200

    Renaming variables in step definitions

--- features/step_definitions/transforms.rb
+++ features/step_definitions/transforms.rb
@@ -2,14 +2,14 @@ CAPTURE_DIMENSIONS = Transform(/^(\d+) x (\d+)$/) do |width, height|
   Dimensions.new(width.to_i, height.to_i)
 end
 
-CAPTURE_POSITION = Transform(/^(\d+) (\d+) [NSEW]$/) do |width, height, letter|
-  Position.new(width.to_i, height.to_i, Orientation.build(letter))
+CAPTURE_POSITION = Transform(/^(\d+) (\d+) ([NSEW])$/) do |width, height, char|
+  Position.new(width.to_i, height.to_i, Orientation.build(char))
 end
 
-CAPTURE_INSTRUCTIONS = Transform(/^[LRM]*$/) do |instructions|
-  instructions.split('').map { |letter| Instruction.build(letter) }
+CAPTURE_INSTRUCTIONS = Transform(/^([LRM])*$/) do |instructions|
+  instructions.split('').map { |char| Instruction.build(char) }
 end
 
-CAPTURE_ROBOT = Transform(/^\w$/) do |robot|
+CAPTURE_ROBOT = Transform(/^\w+$/) do |robot|
   robot
 end
